94f757fd140648e77a62bbb8953ff9e08b7e66b8
lewis <lewisjohns7788@gmail.com>
2024-10-26 08:41:25 +1100

    new code added

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,10 +1,16 @@
 const jwt = require('jsonwebtoken');
+const { GraphQLError } = require('graphql');
 
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
@@ -16,7 +22,7 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req
     }
 
     // verify token and get user data out of it
@@ -25,11 +31,11 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+     
     }
+    return req
 
-    // send to next endpoint
-    next();
+   
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
@@ -39,16 +45,4 @@ module.exports = {
 };
 
 
-//new//
 
-const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas');
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => ({ token: req.headers.authorization })
-});
-
-await server.start();
-server.applyMiddleware({ app });
